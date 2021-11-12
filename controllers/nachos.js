9a5fd3250015f1b0b944f5947d8602484ae1f462
nagamaniyalla <S545424@NWMISSOURI.EDU>
2021-11-12 01:14:28 -0600

    adding in the files

--- controllers/nachos.js
+++ controllers/nachos.js
@@ -13,15 +13,22 @@ exports.nachos_list =exports.nachos_list = async function(req, res) {
 };
  
 // for a specific nachos. 
-exports.nachos_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: nachos detail: ' + req.params.id); 
-}; 
+exports.nachos_detail =  async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await nachos.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
  
 // Handle nachos create on POST. 
 
 exports.nachos_create_post = async function(req, res) { 
     console.log(req.body) 
-    let document = new Costume(); 
+    let document = new nachos(); 
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
@@ -45,9 +52,25 @@ exports.nachos_delete = function(req, res) {
 }; 
  
 // Handle nachos update form on PUT. 
-exports.nachos_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: nachos update PUT' + req.params.id); 
-}; 
+exports.nachos_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await nachos.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.nachos_type)
+    toUpdate.flavour = req.body.flavour;
+    if(req.body.taste) toUpdate.taste = req.body.taste;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   }; 
 // VIEWS 
 // Handle a show all view 
 exports.nachos_view_all_Page = async function(req, res) { 
