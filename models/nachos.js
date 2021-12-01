1b871d03c43e5892b58e21c8bc95748367a9b0a3
nagamaniyalla <S545424@NWMISSOURI.EDU>
2021-11-30 22:50:39 -0600

    adding in the bonus screenshots

--- models/nachos.js
+++ models/nachos.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose") 
-const nachosSchema = mongoose.Schema({flavour: String, taste: String, cost: Number}) 
+const nachosSchema = mongoose.Schema({flavour: String, taste: String, cost: {type:Number,min:15,max:100}}) 
  
 module.exports = mongoose.model("nachos", nachosSchema) 
--- routes/nachos.js
+++ routes/nachos.js
@@ -1,19 +1,40 @@
+// var express = require('express');
+// const nachos_controlers= require('../controllers/nachos'); 
+// var router = express.Router();
+
+// router.get('/', nachos_controlers.nachos_view_all_Page ); 
+
+// /* GET detail nachos page */
+// router.get('/detail', nachos_controlers.nachos_view_one_Page);
+
+// /* GET create nachos page */
+// router.get('/create', nachos_controlers.nachos_create_Page);
+
+// /* GET create update page */
+// router.get('/update', nachos_controlers.nachos_update_Page);
+
+// /* GET create nachos page */
+// router.get('/delete', nachos_controlers.nachos_delete_Page);
+
+// module.exports = router;
 var express = require('express');
-const nachos_controlers= require('../controllers/nachos'); 
+const nachos_controlers= require('../controllers/nachos');
 var router = express.Router();
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
-router.get('/', nachos_controlers.nachos_view_all_Page ); 
-
+router.get('/', nachos_controlers.nachos_view_all_Page );
 /* GET detail nachos page */
 router.get('/detail', nachos_controlers.nachos_view_one_Page);
-
 /* GET create nachos page */
-router.get('/create', nachos_controlers.nachos_create_Page);
-
+router.get('/create',secured, nachos_controlers.nachos_create_Page);
 /* GET create update page */
-router.get('/update', nachos_controlers.nachos_update_Page);
-
+router.get('/update', secured,nachos_controlers.nachos_update_Page);
 /* GET create nachos page */
-router.get('/delete', nachos_controlers.nachos_delete_Page);
-
+router.get('/delete', secured,nachos_controlers.nachos_delete_Page);
 module.exports = router;
